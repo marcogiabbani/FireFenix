4c2e520bf0fa7d2d7fd219d13cbc5f7b80fdb0ec
marco <you@example.com>
2023-02-20 16:06:07 -0300

    changes swipes to vertical ones

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,10 +5,19 @@ function Home() {
   return (
     <div className='h-full flex flex-col justify-between'>
 
-      <div className="snap-x mx-auto snap-mandatory h-[80%] flex w-full overflow-scroll ">
+      {/* <div className="snap-x mx-auto snap-mandatory h-[80%] flex w-full overflow-scroll ">
         {artDisplay.map((painting) => {
           return (
             <div key={painting.id} className="snap-start w-full flex-shrink-0 h-auto flex items-center justify-center px-2 ">
+              <img src={`${painting.content}`} className='max-h-[100%]' />
+            </div>)
+        })}
+      </div> */}
+
+      <div className="overflow-y-scroll snap snap-y snap-mandatory h-[80%] my-auto flex flex-col ">
+        {artDisplay.map((painting) => {
+          return (
+            <div key={painting.id} className="snap-start h-full flex-shrink-0 w-auto flex items-center justify-center px-2 ">
               <img src={`${painting.content}`} className='max-h-[100%]' />
             </div>)
         })}
